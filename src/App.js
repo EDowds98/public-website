b7ee220c745f8632b2094e283e2a522360e1ff5f
EDowds98 <ericdowds1998@gmail.com>
2022-07-25 21:24:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return(
+    <div className="App">
+    <header className="App-header">
+      <h1>
+        Eric Dowds - Aspiring Software Engineer.
+      </h1>
+    </header>
+    <p>This website is currently under construction and will soon be a shiny React app!</p>
+  </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header />
   );
 }
 
