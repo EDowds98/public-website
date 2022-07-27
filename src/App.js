bc8154b15fe717c56696fb557dc4d833737b29e3
EDowds98 <ericdowds1998@gmail.com>
2022-07-27 21:21:22 +0100

    css is hard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import './App.css';
+import React from 'react';
+import { ReactDOM } from 'react';
+import "./output.css";
 
 function Header() {
   return(
-    <div className="App">
-    <header className="App-header">
-      <h1>
-        Eric Dowds - Aspiring Software Engineer.
+    <>
+    <header className="App-header h-40 inline-flex flex-row text-cyan-200 text-6xl p-5 rounded-lg border p-1">
+      <h1 className=''>
+        Eric Dowds -
+        Aspiring Software Engineer.
       </h1>
+    {/*<p className='text-cyan-200 flex-auto mx-auto'>This website is currently under construction and will soon be a shiny React app!</p>*/}
     </header>
-    <p>This website is currently under construction and will soon be a shiny React app!</p>
-  </div>
+  </>
   )
 }
 
+function Photo() {
+  return(
+  <div className='inline-flex flex-row p-1'>
+    <img src='./profile.png' alt='profile' className='flex h-100 w-100
+    items-center justify-center rounded-full border'
+     height="300" width="300"></img>
+  </div> 
+  )}
+
+
 function App() {
   return (
+    <div className='columns-2 gap-5 inline-flex flex-col'>
     <Header />
+    <Photo />
+    </div>
   );
 }
 
