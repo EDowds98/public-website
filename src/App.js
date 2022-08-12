86825bad3864fae460ed3555609c73b5777c148f
EDowds98 <ericdowds1998@gmail.com>
2022-08-12 20:36:13 +0100

    back to vanilla css made it responsive

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import React from 'react';
-import { ReactDOM } from 'react';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image'
+//import { ReactDOM } from 'react';
+import './App.css';
 
 function Header() {
-  return(
+  return (
     <>
-    <header>
-      <h1>
-        Eric Dowds -
-        Aspiring Software Engineer.
-      </h1>
-    {/*<p className='text-cyan-200 flex-auto mx-auto'>This website is currently under construction and will soon be a shiny React app!</p>*/}
-    </header>
-  </>
+      <header>
+        <h1>
+          Eric Dowds -
+          Aspiring Software Engineer.
+        </h1>
+        {/*<p className='text-cyan-200 flex-auto mx-auto'>This website is currently under construction and will soon be a shiny React app!</p>*/}
+      </header>
+    </>
   )
 }
 
 function Photo() {
-  return(
-  <div className='col-md-6'>
-    <Image src='./profile.png' alt='profile'
-     height="300" width="300" fluid roundedCircle/>
-  </div> 
-  )}
+  return (
+    <div>
+      <img src='./profile.png' alt='profile'
+        height="300" width="300" />
+    </div>
+  )
+}
 
 
 function App() {
   return (
-    <Container fluid>
-      <Row>
-        <Col><Header /></Col>
-        <Col><Photo /></Col>
-      </Row>
-    </Container>
+    <div className='App'>
+      <div className='Header'>
+        <Header />
+        <Photo />
+      </div>
+
+      <div className='Main-content'>
+        <p> </p>
+      </div>
+    </div>
   );
 }
 
